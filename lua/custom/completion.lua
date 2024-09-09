55822d166662b9dca4a800f61c6d17fa08bc982e
Justin Benge <jbenge1@arizona.edu>
2024-09-09 15:56:27 +0200

    Apex snippet not loading

--- lua/custom/completion.lua
+++ lua/custom/completion.lua
@@ -31,9 +31,9 @@ cmp.setup {
 }
 
 local ls = require 'luasnip'
--- for _, ft_path in ipairs(vim.api.nvim_get_runtime_file('lua/custom/snippets/*.lua', true)) do
---   loadfile(ft_path)()
--- end
+for _, ft_path in ipairs(vim.api.nvim_get_runtime_file('lua/custom/snippets/*.lua', true)) do
+  loadfile(ft_path)()
+end
 
 vim.keymap.set({ 'i', 's' }, '<C-k>', function()
   if ls.expand_or_jumpable() then
--- lua/custom/snippets/apex.lua
+++ lua/custom/snippets/apex.lua
@@ -1,10 +1,10 @@
 local ls = require 'luasnip'
 
 local s = ls.snippet
-local i = ls.insert_mode
+local i = ls.insert_node
 
 local fmt = require('luasnip.extras.fmt').fmt
 
-ls.add_snippets('apex', {
-  s('ll', fmt('for({} {} : {}{\n})', { i(1), i(2), i(0) })),
+ls.add_snippets('all', {
+  s('ll', fmt('for({} {} : {}){}', { i(1), i(2), i(0), i(3) })),
 })
