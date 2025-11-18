f11e939c3d0bcc9b34030ebefb84fdfc72e18e35
Justin <justinbng36@gmail.com>
2025-11-18 15:51:18 +0100

    Added render markdown

--- init.lua
+++ init.lua
@@ -401,7 +401,7 @@ require('lazy').setup({
         apex_ls = {
           filetypes = { 'apex' },
         },
-        pyright = {},
+        -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
